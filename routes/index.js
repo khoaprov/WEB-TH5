56d5f420a41a58fb46dabeddfb70a9535b5c0e35
khoaprov <gamingxitrum@gmail.com>
2025-03-04 22:06:51 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ const mongoose = require('mongoose');
 var router = express.Router();
 
 /* GET home page. */
-mongoose.connect('mongodb+srv://dikay3012:Khoakhung_3012@cluster0.mluan.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.log(err));
 
 router.get('/product', function(req, res, next) {
   res.render('product', { title: 'Danh sách sản phẩm'});
